bc0900b0d2c7fad6087237ecf852ffd818e99352
dey <deicycmh@gmail.com>
2023-03-17 17:25:57 -0300

    cambio

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,15 +1,37 @@
-class calculadora{
-    sumar(num1,num2){ 
-    return num1+num2
-}
-resta(num1,num2){ 
-return num1-num2
-}
-dividir(num1,num2){
-return num1+num2
-}
-multiplicar(num1,num2){
-return num1*num2
-}
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn");
 
-}
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const botonApretado = boton.textContent;
+
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if (boton.id === "borrar") {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!") {
+                pantalla.textContent = "0";
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if (boton.id === "igual") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+            pantalla.textContent = botonApretado;
+        } else {
+            pantalla.textContent += botonApretado;
+        }
+    })
+})
